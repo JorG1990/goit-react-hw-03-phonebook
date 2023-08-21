dc4a3ec0352f3e118016b634044582a41b3f94cc
JorG1990 <jorge  garcia>
2023-08-21 03:20:09 -0500

    correccion 3

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+
+import PropTypes from 'prop-types';
+import Contact from '../Contact/Contact';
+import { List } from './ContactList.styled';
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <List>
+      {contacts.map(({ id, name, number }) => (
+        <Contact
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDeleteContact={onDeleteContact}
+        />
+      ))}
+    </List>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,9 @@
+
+import styled from "styled-components";
+
+export const List = styled.ul`
+padding: 0 30px;
+flex-direction: column;
+width: 260px;
+display: flex;
+`;
